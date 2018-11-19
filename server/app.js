42c4ec4f056fa687074caa8e544407ec7168f8ca
Hitesh Tare <hiteshtare@gmail.com>
2018-11-19 15:29:22 +0530

    Created Express App in Node and establish  connection with mLab Cloud

---
+++ server/app.js
@@ -0,0 +1,31 @@
+//Node Modules
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+var config = require('./config')
+
+//Initialize express app
+var app = express();
+
+//Port number
+var port = process.env.PORT || 5000
+
+//Enable logging using morgan
+app.use(morgan('dev'));
+
+//Connect to Database
+mongoose.connect(config.mongoURI,{useNewUrlParser: true}).then(() => {
+  console.log(`Connected to ${config.mongoURI}`);
+}).catch((e) => {
+  throw e;
+});
+mongoose.Promise = global.Promise;
+
+//Start express server on specified port
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
+
+
---
+++ server/config/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://trupti:admin123@ds024778.mlab.com:24778/call_notes',
+};
