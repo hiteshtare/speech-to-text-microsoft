627ee936c9ac1489d2880b2afc387fc1cf35fb70
Hitesh Tare <hiteshtare@gmail.com>
2018-11-30 12:53:35 +0530

    Revert "Implemented Async-Await for luis training functionality"
    This reverts commit b628c876f879784c07aac637fd90cbeb3b7c91a9.

--- server/api/controllers/noteController.js
+++ server/api/controllers/noteController.js
@@ -154,74 +154,11 @@ exports.notes_deletes_note = async (req, res, next) => {
     });
   }
 };
-//GET Request for fetching PhraseList associated with id (1460088) for Brands
-async function getBrandsFromPhraseList() {
-  const get_options = {
-    method: 'GET',
-    uri: `${luis_wepApiUri}/phraselists/1460088`,
-    headers: luis_wepApiHeaders
-  };
 
-  const brand_PhraseList = await request(get_options)
-    .then(function (resp) {
-      console.log('GET - Brands Array fetched');
-      let json_resp = JSON.parse(resp);
-      return json_resp["phrases"];
-    });
-
-  return brand_PhraseList;
-}
-
-//PUT Request for updating the string inside PhraseList associated with id (1460088) for Brands
-async function updateBrandsFromPhraseList(p_brand_PhraseList) {
-  const put_options = {
-    method: 'PUT',
-    uri: `${luis_wepApiUri}/phraselists/1460088`,
-    headers: luis_wepApiHeaders,
-    body: {
-      "id": 1460088,
-      "name": "brand",
-      "phrases": p_brand_PhraseList,
-      "isExchangeable": true,
-      "isActive": true
-    },
-    json: true
-  };
-
-  await request(put_options)
-    .then(async function (resp) {
-      console.log('PUT - Brands Array updated');
-
-      await trainLuisApp();
-    });
-}
-
-//POST Request for training luis application
-async function trainLuisApp(p_brand_PhraseList) {
-  const post_options = {
-    method: 'POST',
-    uri: `${luis_wepApiUri}/train`,
-    headers: luis_wepApiHeaders,
-    body: {
-      "id": 1460088,
-      "name": "brand",
-      "phrases": p_brand_PhraseList,
-      "isExchangeable": true,
-      "isActive": true
-    },
-    json: true
-  };
-
-  await request(post_options)
-    .then(function (resp) {
-      console.log('POST - LUIS Trained successfully for new changes');
-    });
-}
-
-exports.train_luis_for_entities = async (req, res, next) => {
+exports.train_luis_for_entities = (req, res, next) => {
   ///////////////////////////PRODUCTS///////////////////////////
-  try {
-    let arr_notes = await Note.find({ // Array of Notes
+  Note
+    .find({
       'entities.products': {
         $elemMatch: {
           'is_approve': true,
@@ -232,89 +169,176 @@ exports.train_luis_for_entities = async (req, res, next) => {
       "entities.products.$": 1
     }, {
       lean: true
-    });
+    })
+    .then((notes) => {
+      var arr_notes = notes; // Array of Notes
+      var arr_products; // Array of Notes
+      ///////////////////////////TRAINING LUIS///////////////////////////
 
-    var arr_products; // Array of Products
-    ///////////////////////////TRAINING LUIS///////////////////////////
-
-    //To check if Array of Notes is empty then return with 'No data found' message
-    if (arr_notes.length == 0) {
-      console.log('MONGO - No Product List Records');
-      res.status(200).json({
-        success: true,
-        message: "No data found for LUIS trainig"
-      });
-      return;
-    }
-
-    //Else proceed further
-    console.log(`MONGO - ${arr_notes.length} Product List fetched for Training`);
-
-    /**********************GET FOR PHRASE LIST FEATURE ARRAY**********************/
-    let brand_PhraseList = await getBrandsFromPhraseList();
-    /**********************GET FOR PHRASE LIST FEATURE ARRAY**********************/
-
-    //Iterate over each note
-    arr_notes.forEach((note) => {
-      arr_products = note["entities"]["products"]; // Initialise Array of Products
-
-      //Iterate over each product inside entities
-      arr_products.forEach((product, index) => {
-        if (index == 0) {
-          console.log(`brand_PhraseList`);
-        }
-        //Replace 'before' sub-string inside string with empty
-        brand_PhraseList = brand_PhraseList.replace(`,${product["before"]}`, '');
-        //Concatinate the string with 'after' sub-string
-        brand_PhraseList += ',' + product["after"];
-        console.log(`Updated : From ${product["before"]} to ${product["after"]}`);
-      }); //End of arr_products.forEach
-    }); //End of arr_notes.forEach
-
-    /**********************UPDATE FOR PHRASE LIST FEATURE ARRAY**********************/
-    await updateBrandsFromPhraseList(brand_PhraseList);
-    /**********************UPDATE FOR PHRASE LIST FEATURE ARRAY**********************/
-
-    /*#####################UPDATING STATUS OF PRODUCT LIST#####################*/
-    console.log('MONGO - Updating status of Product List Trained');
-
-    arr_notes.forEach(async (note) => {
-      arr_products = note["entities"]["products"];
-
-      arr_products.forEach(async (product, index) => {
-        if (index == 0) {
-          console.log(`Updating arr_products`);
-        }
-
-        const id = product["_id"];
-        const updateOps = {
-          "entities.products.$.status": "completed luis training"
-        };
-
-        //Update the note with 'completed luis training' status using ObjectId
-        await Note.updateOne({
-          "entities.products._id": id
-        }, {
-          $set: updateOps
+      if (arr_notes.length == 0) {
+        console.log('MONGO - No Product List Records');
+        res.status(200).json({
+          success: true,
+          message: "No data found for LUIS trainig"
         });
+        return;
+      }
+      console.log(`MONGO - ${arr_notes.length} Product List fetched for Training`);
+      /**********************GET FOR PHRASE LIST FEATURE ARRAY**********************/
+      const get_options = {
+        method: 'GET',
+        uri: `${luis_wepApiUri}/phraselists/1460088`,
+        headers: luis_wepApiHeaders
+      };
 
-        console.log(`Mongo - Updated product : ${id} successfully`);
-      }); //End of arr_products.forEach
-    }); //End of arr_notes.forEach
-    /*#####################UPDATING STATUS OF PRODUCT LIST#####################*/
+      request(get_options)
+        .then(function (resp) {
+          console.log('GET - Brands Array fetched');
+          let json_resp = JSON.parse(resp);
+          let brand_PhraseList = json_resp["phrases"];
 
-    res.status(200).json({
-      success: true,
-      message: "LUIS Trained successfully for new changes"
+          arr_notes.forEach((note) => {
+            arr_products = note["entities"]["products"];
+
+            arr_products.forEach((product, index) => {
+              if (index == 0) {
+                console.log(`brand_PhraseList`);
+              }
+              brand_PhraseList = brand_PhraseList.replace(`,${product["before"]}`, '');
+              brand_PhraseList += ',' + product["after"];
+              console.log(`Updated : From ${product["before"]} to ${product["after"]}`);
+            });
+          });
+
+          /**********************PUT FOR PHRASE LIST FEATURE ARRAY**********************/
+          const put_options = {
+            method: 'PUT',
+            uri: `${luis_wepApiUri}/phraselists/1460088`,
+            headers: luis_wepApiHeaders,
+            body: {
+              "id": 1460088,
+              "name": "brand",
+              "phrases": brand_PhraseList,
+              "isExchangeable": true,
+              "isActive": true
+            },
+            json: true
+          };
+
+          request(put_options)
+            .then(function (resp) {
+              console.log('PUT - Brands Array updated');
+
+              /**********************POST FOR TRAIN APPLICATION VERSION**********************/
+              const post_options = {
+                method: 'POST',
+                uri: `${luis_wepApiUri}/train`,
+                headers: luis_wepApiHeaders,
+                body: {
+                  "id": 1460088,
+                  "name": "brand",
+                  "phrases": brand_PhraseList,
+                  "isExchangeable": true,
+                  "isActive": true
+                },
+                json: true
+              };
+
+              request(post_options)
+                .then(function (resp) {
+                  console.log('POST - LUIS Trained successfully for new changes');
+
+                  /*#####################UPDATING STATUS OF PRODUCT LIST#####################*/
+                  console.log('MONGO - Updating status of Product List Trained');
+
+                  arr_notes.forEach((note) => {
+                    arr_products = note["entities"]["products"];
+
+                    arr_products.forEach((product, index) => {
+                      if (index == 0) {
+                        console.log(`Updating arr_products`);
+                      }
+
+                      const id = product["_id"];
+                      const updateOps = {
+                        "entities.products.$.status": "completed luis training"
+                      };
+
+                      Note.updateOne({
+                          "entities.products._id": id
+                        }, {
+                          $set: updateOps
+                        })
+                        .then(note => {
+                          console.log(`Mongo - Updated product : ${id} successfully`);
+                        })
+                        .catch(err => {
+                          res.status(500).json({
+                            success: false,
+                            message: err
+                          });
+                        });
+                    });
+
+                  });
+                  /*#####################UPDATING STATUS OF PRODUCT LIST#####################*/
+
+                  res.status(200).json({
+                    success: true,
+                    message: "LUIS Trained successfully for new changes"
+                  });
+                })
+                .catch(function (err) {
+                  // Deal with the error
+                });
+              /**********************POST FOR TRAIN APPLICATION VERSION**********************/
+            })
+            .catch(function (err) {
+              // Deal with the error
+            });
+          /**********************PUT FOR PHRASE LIST FEATURE ARRAY**********************/
+        })
+        .catch(function (err) {
+          // Deal with the error
+        });
+      /**********************GET FOR PHRASE LIST FEATURE ARRAY**********************/
+
+      ///////////////////////////TRAINING LUIS///////////////////////////
+
+      ///////////////////////////UPDATING STATUS///////////////////////////
+      // updateOps = {
+      //   "entities.products.$.status": 'completed luis traning'
+      // };
+
+      // Note.update({
+      //     "entities.products._id": ObjectId("5bf52eec1af1ab1a49aa0b9b")
+      //   }, {
+      //     $set: updateOps
+      //   })
+      //   .then(note => {
+      //     res.status(200).json({
+      //       success: true,
+      //       message: "Products : LUIS training completed successfully",
+      //       payload: note
+
+      //     });
+      //   })
+      //   .catch(err => {
+      //     res.status(500).json({
+      //       success: false,
+      //       message: err
+
+      //     });
+      //   });
+      ///////////////////////////UPDATING STATUS///////////////////////////
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        success: false,
+        message: err
+      });
     });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      success: false,
-      message: err
-    });
-  }
-
   // ///////////////////////////KEYMESSAGES///////////////////////////
   // Note
   //   .find({
