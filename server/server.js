0492f5b678fa36512422ae09819fe9e1196760dd
Hitesh Tare <hiteshtare@gmail.com>
2018-12-07 10:31:22 +0530

    Change for Heroku

--- server/server.js
+++ server/server.js
@@ -2,11 +2,12 @@ const http = require('http');
 const app = require('./app');
 
 //Port number
+console.log(`Before port`);
 var port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
-
+console.log(`After server`);
 //Start express server on specified port
-server.listen(process.env.PORT || port, () => {
+server.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
 });
